ae10b43b9c3f51a72be4fcf064b7a27b53c40e86
Akhil Kumar <akhil.se2024@gmail.com>
2024-12-08 03:03:08 +0530

    AGain updating the protected Routes

--- frontend/src/components/ProtectedRoutes/ProtectedRoutes.jsx
+++ frontend/src/components/ProtectedRoutes/ProtectedRoutes.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from './AuthContext';
 
-const ProtectedRoute = ({ children ,roles}) => {
+const ProtectedRoute = ({ children ,allowedRoles=[]}) => {
   const { user } = useAuth();
 
   // Debugging: Log user and roles
   console.log("ProtectedRoute: User is", user);
-  console.log("ProtectedRoute: Roles required", roles);
+  console.log("ProtectedRoute: Roles required", allowedRoles);
 
   if (!user) {
     console.warn("User not authenticated. Redirecting to login...");
     return <Navigate to="/" />;
   }
 
-  if (roles.length > 0 && !roles.includes(user.role)) {
+  if (allowedRoles.length > 0 && !allowedRoles.includes(user.role)) {
     console.warn(`User role (${user.role}) is not authorized. Redirecting to unauthorized page...`);
     return <Navigate to="/unauthorized" />;
   }
