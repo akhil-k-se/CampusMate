187ff5ac889e36d0a1d0ff512cfd33031938e509
Akhil Kumar <akhil.se2024@gmail.com>
2024-12-08 17:22:27 +0530

    Checking for protected routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -38,61 +38,198 @@ import Dashboard1 from "./components/superadmin/Dashboard/Dashboard1";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Public Routes */}
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/select-user" element={<AdminOrUser />} />
-        <Route path="/student-signup" element={<RegisterLogin />} />
-        <Route path="/warden-signup" element={<AdminLogin />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/super-admin/login" element={<SuperLogin />} />
+    <AuthProvider>
+      <Router>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/select-user" element={<AdminOrUser />} />
+          <Route path="/student-signup" element={<RegisterLogin />} />
+          <Route path="/warden-signup" element={<AdminLogin />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/super-admin/login" element={<SuperLogin />} />
 
-        {/* Student Routes */}
-        <Route path="/student" element={<StudentLandingPage />} />
-        <Route path="/student/my-gatepasses" element={<Studentgatepass />} />
-        <Route path="/student/my-complaints" element={<StudentComplaints />} />
-        <Route path="/student/booking/payment" element={<PaymentPage />} />
-        <Route path="/student/QR" element={<QRcode />} />
-        <Route path="/student/mess" element={<UserMess />} />
+          {/* Student Routes */}
+          <Route
+            path="/student"
+            element={
+              <ProtectedRoute allowedRoles={["student"]}>
+                <StudentLandingPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/student/my-gatepasses"
+            element={
+              <ProtectedRoute allowedRoles={["student"]}>
+                <Studentgatepass />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/student/my-complaints"
+            element={
+              <ProtectedRoute allowedRoles={["student"]}>
+                <StudentComplaints />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/student/booking/payment"
+            element={
+              <ProtectedRoute allowedRoles={["student"]}>
+                <PaymentPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/student/QR"
+            element={
+              <ProtectedRoute allowedRoles={["student"]}>
+                <QRcode />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/student/mess"
+            element={
+              <ProtectedRoute allowedRoles={["student"]}>
+                <UserMess />
+              </ProtectedRoute>
+            }
+          />
 
-        {/* Warden Routes */}
-        <Route path="/warden/dashboard" element={<Dashboard />} />
-        <Route path="/warden/student-details" element={<Studentdetails />} />
-        <Route path="/warden/gatepasses" element={<Gatepassdetails />} />
-        <Route path="/warden/complaints" element={<Complaintbox />} />
-        <Route path="/warden/my-account" element={<Account />} />
-        <Route path="/warden/update/mess-menu" element={<MessMenu />} />
+          {/* Warden Routes */}
+          <Route
+            path="/warden/dashboard"
+            element={
+              <ProtectedRoute allowedRoles={["warden"]}>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/warden/student-details"
+            element={
+              <ProtectedRoute allowedRoles={["warden"]}>
+                <Studentdetails />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/warden/gatepasses"
+            element={
+              <ProtectedRoute allowedRoles={["warden"]}>
+                <Gatepassdetails />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/warden/complaints"
+            element={
+              <ProtectedRoute allowedRoles={["warden"]}>
+                <Complaintbox />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/warden/my-account"
+            element={
+              <ProtectedRoute allowedRoles={["warden"]}>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/warden/update/mess-menu"
+            element={
+              <ProtectedRoute allowedRoles={["warden"]}>
+                <MessMenu />
+              </ProtectedRoute>
+            }
+          />
 
-        {/* Mess Guard Routes */}
-        <Route path="/guard/mess-login" element={<MessLogin />} />
-        <Route path="/guard/scanner" element={<QRScanner />} />
+          {/* Mess Guard Routes */}
+          <Route
+            path="/guard/mess-login"
+            element={
+              <ProtectedRoute allowedRoles={["mess-guard"]}>
+                <MessLogin />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/guard/scanner"
+            element={
+              <ProtectedRoute allowedRoles={["mess-guard", "gate-security"]}>
+                <QRScanner />
+              </ProtectedRoute>
+            }
+          />
 
-        {/* Gate Security Routes */}
-        <Route path="/guard/gate-login" element={<GateLogin />} />
+          {/* Gate Security Routes */}
+          <Route
+            path="/guard/gate-login"
+            element={
+              <ProtectedRoute allowedRoles={["gate-security"]}>
+                <GateLogin />
+              </ProtectedRoute>
+            }
+          />
 
-        {/* Super Admin Routes */}
-        <Route
-          path="/super-admin/create/mess-security"
-          element={<MessSign />}
-        />
+          {/* Super Admin Routes */}
+          <Route
+            path="/super-admin/create/mess-security"
+            element={
+              <ProtectedRoute allowedRoles={["super-admin"]}>
+                <MessSign />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/super-admin/details/wardens"
-          element={<Wardendetails />}
-        />
-        <Route path="/super-admin/create/warden" element={<Warden />} />
-        <Route
-          path="/super-admin/create/gate-security"
-          element={<GateSecurity />}
-        />
-        <Route
-          path="/super-admin/details/gate-security"
-          element={<GuardDetails />}
-        />
-        <Route path="/super-admin/dashboard" element={<Dashboard1 />} />
-      </Routes>
-    </Router>
+          <Route
+            path="/super-admin/details/wardens"
+            element={
+              <ProtectedRoute allowedRoles={["super-admin"]}>
+                <Wardendetails />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/super-admin/create/warden"
+            element={
+              <ProtectedRoute allowedRoles={["super-admin"]}>
+                <Warden />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/super-admin/create/gate-security"
+            element={
+              <ProtectedRoute allowedRoles={["super-admin"]}>
+                <GateSecurity />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/super-admin/details/gate-security"
+            element={
+              <ProtectedRoute allowedRoles={["super-admin"]}>
+                <GuardDetails />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/super-admin/dashboard"
+            element={
+              <ProtectedRoute allowedRoles={["super-admin"]}>
+                <Dashboard1 />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
